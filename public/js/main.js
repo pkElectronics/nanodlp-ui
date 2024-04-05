13d4d0d31e01517fb1f2e9ec4a397c8d28f6413a
Shahin <shahinv@gmx.net>
2024-04-06 03:13:27 +0330

    Revert "Update resources to the latest nanodlp build"
    This reverts commit 4d602fd6644c8e5b9dfe7ee288350826ff2cfd0e.

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ $(function(){
 	// Run for index page
 	display_notification();
 	notification_close();
-	setInterval(function(){display_notification();}, 8000);
+	setTimeout(function(){display_notification();}, 2000);
 	if($('#buzzer').length>0){
 		display_console_log();
 		setInterval(function(){display_console_log();}, 3000);
@@ -584,13 +584,10 @@ function update_status(){
 				$(".printing-obj").slideDown();
 			}
 			layer_progress(data['PrevLayerTime'],data['LayerStartTime']);
-			update_stat();
 		}
 		if ($("#stat").length>0){
 			change_stats(data,['proc','disk','mem','uptime','proc_numb','temp', 'resin']);
 		}
-		var log=$.parseJSON(data["log"]);
-		last_value('msg',log['msg']);
 		update_timeline();
 		current_status_display();
 
@@ -607,9 +604,9 @@ function update_status(){
 	});
 }
 
-async function update_stat(){
+function update_stat(){
 	$.ajax({
-		url:'/stat',
+		url:'stat',
 		dataType: 'json',
 		type: 'GET',
 		timeout: 1200
@@ -647,14 +644,10 @@ function change_stats(data,keys){
 	});
 }
 
+display_notification.prev_msg='';
 display_notification.prev_data='';
-display_notification.modal='';
-display_notification.notification='';
-function display_notification(){
-	if (display_notification.notification==""){
-		display_notification.notification=$("#notification-template").html();
-		display_notification.modal=$("#modal-template").html();
-	}
+
+function display_notification(){	
 	$.ajax({
 		url:'/notification',
 		type: 'GET',
@@ -664,8 +657,8 @@ function display_notification(){
 		if (data===null) return;
 
 		if(data.length > 0){
-		$.each(data,function(k,v){
-			if( display_notification.prev_data === "" || v['Timestamp'] >= display_notification.prev_data['Timestamp']){
+			$.each(data,function(k,v){		
+				if( display_notification.prev_data === "" || v['Timestamp'] >= display_notification.prev_data['Timestamp']){
 					
 					var date = new Date();
 					date.setTime(v['Timestamp']*1000);
@@ -711,7 +704,7 @@ function display_notification(){
 			display_notification.prev_msg=msg;
 			$("#notificationModal").remove();
 			$(".navbar").after(msg);
-		
+
 
 			$('#btn-modal-continue').click(function(){
 				try {     
@@ -752,7 +745,7 @@ function display_notification(){
 			$('#notificationModal').modal('hide');
 			$("#notificationModal").remove();
 		}
-
+		
 		
 
 
@@ -787,7 +780,7 @@ Modal content...
 
 function notification_close(){
 	$("body").delegate('.notification-service button','click', function (e) {
-		$.get("/notification/disable/"+$(this).parents(".notification-service").data("notification"));
+		$.get("/notification/disable/"+$(this).parent().data("notification"));
 	});
 }
 
@@ -1042,7 +1035,7 @@ $("#expertModeCheckbox").click(function (e) {
 		type: "GET",
 		dataType: "json",
 		complete: () => { 
-		window.location.reload(true);
+			window.location.reload(true);
 		},
  	 });
 });
