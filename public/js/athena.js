fcc59d497e0d6a4b904512cf535906b77123f677
pkElectronics <pascalkrumme@gmail.com>
2025-06-24 21:24:26 +0200

    Update athena.js
    Add specific accept headers to upload xhr

--- public/js/athena.js
+++ public/js/athena.js
@@ -842,6 +842,8 @@ $('.upload-disable').submit(function(e) {
 		upload_xhr = new XMLHttpRequest();
 		upload_xhr.open("POST", "/plate/add");
 
+		//
+		upload_xhr.setRequestHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
 
 		upload_xhr.upload.addEventListener("progress", progressHandler, false);
 		//upload_xhr.addEventListener("loadend", completeHandler, false);
